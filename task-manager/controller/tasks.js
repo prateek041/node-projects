df62dc02e1aa4091fc78ea2836bbcc6f84aa9c42
prateek041 <prateeksingh9741@gmail.com>
2022-07-14 15:03:49 +0530

    all route setup

--- task-manager/controller/tasks.js
+++ task-manager/controller/tasks.js
@@ -4,6 +4,26 @@ const getAllTasks = (req, res) => {
     res.send('all items from the controller')
 }
 
+const createTask = (req, res) => {
+    res.send('New task created')
+}
+
+const getTask = (req, res) => {
+    res.send('your single task')
+}
+
+const updateTask = (req, res) => {
+    res.send('task updatad')
+}
+
+const deleteTask = (req, res) => {
+    res.send('task deleted')
+}
+
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
