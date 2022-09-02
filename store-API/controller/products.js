61535341c3734e44890a20c7aecb6efb9115072e
prateek041 <prateeksingh9741@gmail.com>
2022-09-02 20:59:06 +0530

    new origin

--- store-API/controller/products.js
+++ store-API/controller/products.js
@@ -3,7 +3,7 @@ const Products = require('../model/products')
 
 const getAllProductsStatic = async (req, res) => {
     // we can use regex to search query things that match some of the required criterias.
-    const products = await Products.find({}).sort('-name');
+    const products = await Products.find({ price: { $gt: 30 } }).select('name price').limit(4);
     res.status(200).send({
         "status": "success",
         "Data": products,
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     // we will be creating a query object, that will query the data from DB. It looks for pattern
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -28,12 +28,27 @@ const getAllProducts = async (req, res) => {
 
     let result = Products.find(queryObject); // we need to get the objects based on the query.
 
+    // sort
     if (sort) { // once we get the result, we will check if it needs to be sorted.
         // products = products.sort()
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
     }
-    const products = await result; // finally we have the project.
+    if (fields) {
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList)
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit; // special logic, look into it.
+
+    result = result.skip(skip).limit(limit)
+
+    // finalising the output.
+    const products = await result; // finally we have the products.
     res.status(200).send({
         "status": "success",
         products,
