7b08ebbee962a2fbd52515e3cd4ace285f64bacc
prateek041 <prateeksingh9741@gmail.com>
2022-09-02 14:02:40 +0530

    cover up commit

--- store-API/controller/products.js
+++ store-API/controller/products.js
@@ -3,7 +3,7 @@ const Products = require('../model/products')
 
 const getAllProductsStatic = async (req, res) => {
     // we can use regex to search query things that match some of the required criterias.
-    const products = await Products.find({ name: "accent chair" });
+    const products = await Products.find({}).sort('-name');
     res.status(200).send({
         "status": "success",
         "Data": products,
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     // we will be creating a query object, that will query the data from DB. It looks for pattern
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -24,8 +24,16 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' }; // case insensitive
     }
-    console.log(queryObject)
-    const products = await Products.find(queryObject)
+    // console.log(queryObject)
+
+    let result = Products.find(queryObject); // we need to get the objects based on the query.
+
+    if (sort) { // once we get the result, we will check if it needs to be sorted.
+        // products = products.sort()
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    }
+    const products = await result; // finally we have the project.
     res.status(200).send({
         "status": "success",
         products,
